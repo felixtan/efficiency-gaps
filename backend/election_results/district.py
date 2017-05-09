6b9308db8832ebd383204474dd673b845943cec3
felixtan <tanf91@gmail.com>
2017-05-09 00:23:13 -0400

    added docstrings for DistrictElectionResults

--- backend/election_results/district.py
+++ backend/election_results/district.py
@@ -5,10 +5,33 @@ from warnings import warn
 
 class DistrictElectionResults:
     """Stores election results for a legislative district
+
+    Attributes:
+        year (Int) - The election year
+        state (String) - Two-letter abbreviation of a US state
+        legislative_body_code (Int) - Int that maps to a legislative body.
+            See ../fixtures/legislative_body_codes.py
+        district (Int) - Legislative district number
+        votes_dem (Int) - Number of votes for the Democratic candidate
+        votes_rep (Int) - Number of votes for the Republican candidate
+        votes_other (Int) - Number of votes for a third-party or independent
+        votes_voided (Int) - Number of votes that were invalid and not applied to
+            any candidate
+        votes_total (Int) - Number of total votes in the election
+        votes_wasted_dem (Int) - Number of votes wasted by the Democratic candidate
+        votes_wasted_rep (Int) - Number of votes wasted by the Republican candidate
+        votes_wasted_net (Int) - The difference in wasted votes between the
+            Democratic and republican candidates
+        winner (Dict) - Contains party (three-letter lowercase abbreviation),
+            last_name and first_name of the winner
     """
 
     def __init__(self, year, state, legislative_body_code, district, data=None):
+        """Instantiate a DistrictElectionResults object
 
+        Attibutes:
+            data (Dict) - Contains the non-argument properties
+        """
         try:
             if isinstance(year, int):
                 self.year = year
@@ -61,23 +84,34 @@ class DistrictElectionResults:
         }
 
     def is_valid_state(self, state_abbrev):
+        """Returns true if state_abbrev is a US state abbreviation, else false
+
+           See ../fixtures/states.py
+        """
         if not isinstance(state_abbrev, str):
             raise TypeError("state abbrev has invalid type {}, must be str".format(type(state_abbrev)))
 
         if len(state_abbrev) > 2:
             raise NameError("Invalid state abbrev: {}, must be 2 letters".format(state_abbrev))
 
-        # states = json.load(states_json)
-        return state_abbrev in states.keys()
+        return state_abbrev.upper() in states.keys()
 
     def is_valid_legislative_body_code(self, code):
+        """Returns true if code maps to a legislative body, else false
+
+           See ../fixtures/legislative_body_codes.py
+        """
         if not isinstance(code, int):
             raise TypeError("legislative body code has invalid type {}".format(type(code)))
 
-        # bodies = json.load(legislative_body_codes_json)
         return code in legislative_body_codes.keys()
 
     def calc_wasted_votes(self, votes_rep, votes_dem, votes_total):
+        """Calculates the wasted votes of Republican and Democratic candidates
+
+           A wasted vote is defined as one for the losing candidate or one for
+           the winning candidate in excess of the number needed to win.
+        """
         if votes_total > votes_rep + votes_dem:
             votes_total = votes_rep + votes_dem
             warn("Total votes is greater than the sum of votes for Reps and Dems")
