b6d94bd111d1bbcb6473ac49ea39393cfd3287c2
felixtan <tanf91@gmail.com>
2017-05-10 20:22:12 -0400

    tests and implementation of ElectionResults errors

--- backend/election_results/tests/test_utils.py
+++ backend/election_results/tests/test_utils.py
@@ -1,6 +1,51 @@
 import unittest
 import election_results.utils as utils
-from election_results.election_results import ElectionResults as Results
 
 class TestElectionResultsUtils(unittest.TestCase):
-    pass
+
+    def test_VotesError_requires_all_arguments(self):
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_rep=0, votes_total=0,
+            year=0, state='NY', legislative_body_code=0)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_rep=0, votes_total=0,
+            year=0, state='NY', district=1)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_rep=0, votes_total=0,
+            year=0, legislative_body_code=0, district=1)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_rep=0, votes_total=0,
+            state='NY', legislative_body_code=0, district=1)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_rep=0,
+            year=0, state='NY', legislative_body_code=0, district=1)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_dem=0, votes_total=0,
+            year=0, state='NY', legislative_body_code=0, district=1)
+        self.assertRaises(TypeError, utils.VotesError,
+            votes_rep=0, votes_total=0,
+            year=0, state='NY', legislative_body_code=0, district=1)
+
+    def test_VotesError_stores_information_about_the_error(self):
+        e = utils.VotesError(votes_dem=0, votes_rep=0, votes_total=0,
+            year=0, state='NY', legislative_body_code=0, district=1)
+
+        self.assertEqual(e.votes_dem, 0)
+        self.assertEqual(e.votes_rep, 0)
+        self.assertEqual(e.votes_total, 0)
+        self.assertEqual(e.year, 0)
+        self.assertEqual(e.state, 'NY')
+        self.assertEqual(e.legislative_body_code, 0)
+        self.assertEqual(e.district, 1)
+
+    def test_USStateError_stores_information_about_the_error(self):
+        e = utils.USStateError(state='NT')
+        self.assertEqual(e.state, 'NT')
+
+    def test_LegislativeBodyError_stores_information_about_the_error(self):
+        e = utils.LegislativeBodyError(legislative_body_code=3)
+        self.assertEqual(e.legislative_body_code, 3)
+
+    def test_DistrictError_stores_information_about_the_error(self):
+        e = utils.DistrictError(district=-1)
+        self.assertEqual(e.district, -1)
--- backend/election_results/utils.py
+++ backend/election_results/utils.py
@@ -14,3 +14,61 @@ def set_year_if_valid(results, year):
             raise TypeError("Year has invalid type {}".format(type(year)))
     except TypeError as error:
         raise error
+
+class ElectionResultsError(Exception):
+    """Exception for errors raise by ElectionResults"""
+    def __init__(self, msg):
+        super(ElectionResultsError, self).__init__(msg)
+
+class VotesError(ElectionResultsError):
+    """Exception for errors raised in counting votes in election results
+    """
+
+    def __init__(self, votes_dem, votes_rep, votes_total,
+        year, state, legislative_body_code, district):
+
+        msg = "Sum of votes for dem(={dem}) and rep(={rep}) is greater than \
+            total(={total})".format(dem=votes_dem, rep=votes_rep, total=votes_total)
+
+        super(VotesError, self).__init__(msg)
+
+        self.votes_dem = votes_dem
+        self.votes_rep = votes_rep
+        self.votes_total = votes_total
+        self.year = year
+        self.state = state
+        self.legislative_body_code = legislative_body_code
+        self.district = district
+
+class USStateError(ElectionResultsError):
+    """Exception for errors raised in validating US states
+    """
+
+    def __init__(self, state):
+        msg = "There is no such state with the abbreviation {}".format(state)
+        super(USStateError, self).__init__(msg)
+        self.state = state
+
+class LegislativeBodyError(ElectionResultsError):
+    """Exception for errors raised in validating legislative body code
+    """
+
+    def __init__(self, legislative_body_code):
+        msg = """Invalid legislative body code {}
+            Valid options
+                0 - US House of Representatives
+                1 - State Legislature Upper House
+                2 - State Legislature Lower House
+        """.format(legislative_body_code)
+
+        super(LegislativeBodyError, self).__init__(msg)
+        self.legislative_body_code = legislative_body_code
+
+class DistrictError(ElectionResultsError):
+    """Exception for errors raised in validating district numbers
+    """
+
+    def __init__(self, district):
+        msg = "Invalid district number {}".format(district)
+        super(DistrictError, self).__init__(msg)
+        self.district = district
